178180e544d49c26a44f8580e136674b2c3bae83
Greyarch BV <gantcho@greyarch.net>
2018-01-05 12:38:40 +0100

    simplified services type - all complex level 2 sub fields are of type JSON or array of JSON

--- src/schema/types/services.js
+++ src/schema/types/services.js
@@ -9,63 +9,15 @@ export default `
   type ServiceSpec {
     Name: String,
     Labels: JSON
-    Mode: ServiceMode
-    TaskTemplate: ServiceTaskTemplate
-    UpdateConfig: ServiceSpecRollbackUpdateConfig
-    RollbackConfig: ServiceSpecRollbackUpdateConfig
-    EndpointSpec: ServiceEndpointSpec
+    Mode: JSON
+    TaskTemplate: JSON
+    UpdateConfig: JSON
+    RollbackConfig: JSON
+    EndpointSpec: JSON
   }
-  type ServiceTaskTemplate {
-    ContainerSpec: ServiceContainerSpec
-    Resources: ServiceResources
-    RestartPolicy: ServiceTaskTemplateRestartPolicy
-    Placement: ServicePlacement
-    ForceUpdate: Int
-    Runtime: String
-  }
-  type ServiceContainerSpec {
-    Image: String,
-    Args: [String],
-    DNSConfig: JSON,
-    Isolation: String
-  }
-  type ServiceResources {
-    Limits: JSON,
-    Reservations: JSON
-  }
-  type ServiceTaskTemplateRestartPolicy {
-    Condition: String
-    MaxAttempts: Int
-  }
-  type ServicePlacement {
-    Platforms: [ ServicePlacementPlatform ]
-  }
-  type ServicePlacementPlatform { Architecture: String, OS: String }
-  type ServiceMode { Replicated: ServiceModeReplicated }
-  type ServiceModeReplicated { Replicas: Int }
   type ServiceEndpoint {
-    Spec: ServiceEndpointSpec
-    Ports: [ServiceEndpointPort]
-    VirtualIPs: [ServiceEndpointVirtualIP]
-  }
-  type ServiceEndpointSpec {
-    Mode: String
-    Ports: [ServiceEndpointPort]
-  }
-  type ServiceEndpointPort {
-    Protocol: String
-    TargetPort: Int
-    PublishedPort: Int
-  }
-  type ServiceEndpointVirtualIP {
-    NetworkID: String
-    Addr: String
-  }
-  type ServiceSpecRollbackUpdateConfig {
-    Parallelism: Int
-    Delay: Int
-    FailureAction: String
-    Monitor: Int
-    MaxFailureRatio: Float
+    Spec: JSON
+    Ports: [JSON]
+    VirtualIPs: [JSON]
   }
 `;
